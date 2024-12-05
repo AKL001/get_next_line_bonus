5a8cf7514ac981af765502e3e0f6c6d2b7af61f6
AKL001 <l9udyr@gmail.com>
2024-12-05 10:46:30 +0100

    New-get_next_line

---
+++ get_next_line_utils.c
@@ -0,0 +1,115 @@
+#include "get_next_line.h"
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		len = 0;
+	else if (len > s_len - start || len > s_len)
+		len = s_len - start;
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	ft_memcpy(ptr, s + start, len);
+	ptr[len] = '\0';
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+char	*ft_memcpy(char *dest,const char *src, size_t n)
+{
+	size_t	i;
+
+	if (dest == src)
+		return (NULL);
+	if (!dest && !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		*((char *)(dest + i)) = *((char *)(src + i));
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	max_len;
+	char	*ptr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	max_len = s1_len + s2_len;
+	ptr = (char *)malloc(sizeof(char) * (max_len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	ft_memmove(ptr, s1, s1_len);
+	ft_memmove(ptr + s1_len, s2, s2_len);
+	ptr[max_len] = '\0';
+	return (ptr);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	if (dest == src)
+		return (dest);
+	if (!dest && !src)
+		return (NULL);
+	if (dest < src)
+		ft_memcpy(dest, src, n);
+	else
+	{
+		while (n--)
+		{
+			*((unsigned char *)(dest + n)) = *((unsigned char *)(src + n));
+		}
+	}
+	return (dest);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*dst;
+
+	len = ft_strlen(s1);
+	dst = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dst)
+		return (0);
+	ft_memcpy(dst, s1, len);
+	dst[len] = '\0';
+	return (dst);
+}
